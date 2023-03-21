37b1546242ccff516852b560c047b7e8cf19f649
Aakanksha Gupta <aakanksha.gupta01@sap.com>
2023-03-20 17:09:31 -0700

    dashboard draft template

---
+++ src/components/Sidebar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { slide as Menu } from 'react-burger-menu';
+import './Sidebar.css';
+
+export default function Sidebar() {
+    return (
+        <Menu>
+                <a className="menu-item" href="/">
+                    Dashboard
+                </a>
+                <a className="menu-item" href="/listproj">
+                    Project
+                </a>
+                <a className="menu-item" href="/node">
+                    Node
+                </a>
+        </Menu>
+    );
+};
